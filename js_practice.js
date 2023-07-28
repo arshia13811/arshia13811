2a2e26ba9e38dbacc2084d7d49243ce70c2e4eee
arshia13811 <85649767+arshia13811@users.noreply.github.com>
2023-07-28 19:14:50 +0900

    aa

--- js_practice.js
+++ js_practice.js
@@ -1,7 +1,5 @@
-const numbers = [1, -1, 2, 3]
-
-let sum = numbers.reduce(function(accumulator, currentValue){
-    return accumulator + currentValue;
-} , 0)
-
-console.log(sum);
+function interest(principal, rate= 3.5, years = 5){
+    return((principal * rate)/100) * years; 
+}
+// if the user did not pass an arg then take a default value
+console.log(interest(100000, 3.5, 5));
