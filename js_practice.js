ae5f7d62353a4b4d24c94cb95a4286ba616630df
arshia13811 <85649767+arshia13811@users.noreply.github.com>
2023-07-29 00:18:09 +0900

    Update js_practice.js

---
+++ js_practice.js
@@ -0,0 +1,23 @@
+// shape
+// base class, super class, parent class are the same
+
+
+
+// circle , square
+// derived class, sub class, child class are the same
+
+// circle IS-A shape
+
+
+
+// if circle inherits from shape then we say that shape is circles prototype
+
+// all objects in js has a prototype/father
+
+
+let person = {name : 'mohamd'}
+console.log(person);
+
+for(let key in person){
+    console.log(Object.keys(person))
+}
